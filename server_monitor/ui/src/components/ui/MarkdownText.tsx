761657c1e2a45c26178c55d1c5acf0c376b207d9
Andrew <aecobb53@gmail.com>
2026-09-07 18:51:14 +0000

    saving

--- server_monitor/ui/src/components/ui/MarkdownText.tsx
+++ server_monitor/ui/src/components/ui/MarkdownText.tsx
@@ -12,6 +12,7 @@ interface MarkdownTextProps {
 type MarkdownNode = {
   type?: string;
   value?: string;
+  url?: string;
   children?: MarkdownNode[];
   data?: {
     hName?: string;
@@ -77,7 +78,7 @@ function remarkSubSuper() {
         }
 
         const text = child.value;
-        const pattern = /\[color:([a-z_]+)(\+icon)?\]([^\n]+?)\[\/color\]|\[highlight:(yellow|green|blue|red|orange)\]([^\n]+?)\[\/highlight\]|(?<!\^)\^([^\^\n]+)\^(?!\^)|(?<!~)~([^~\n]+)~(?!~)/g;
+        const pattern = /\[\[color:([a-z_]+)(\+icon)?\]([^\n\]]+?)\[\/color\]\]\(([^)\s]+)\)|\[color:([a-z_]+)(\+icon)?\]([^\n]+?)\[\/color\]|\[highlight:(yellow|green|blue|red|orange)\]([^\n]+?)\[\/highlight\]|(?<!\^)\^([^\^\n]+)\^(?!\^)|(?<!~)~([^~\n]+)~(?!~)/g;
         let lastIndex = 0;
         let match: RegExpExecArray | null;
 
@@ -86,7 +87,7 @@ function remarkSubSuper() {
             nextChildren.push({ type: 'text', value: text.slice(lastIndex, match.index) });
           }
 
-          if (match[1] && match[3]) {
+          if (match[1] && match[3] && match[4]) {
             const token = match[1].toLowerCase();
             const iconRequested = Boolean(match[2]);
 
@@ -102,26 +103,50 @@ function remarkSubSuper() {
             }
 
             nextChildren.push({
+              type: 'link',
+              url: match[4],
+              children: [{
+                type: 'colored_text',
+                children: [{ type: 'text', value: match[3] }],
+                data: { hName: 'span', hProperties: { className: classNames } },
+              }],
+            });
+          } else if (match[5] && match[7]) {
+            const token = match[5].toLowerCase();
+            const iconRequested = Boolean(match[6]);
+
+            if (!COLOR_TOKENS.has(token)) {
+              nextChildren.push({ type: 'text', value: match[0] });
+              lastIndex = pattern.lastIndex;
+              return;
+            }
+
+            const classNames = [`md-color-${token}`];
+            if (iconRequested && ICON_TOKENS.has(token)) {
+              classNames.push('md-with-icon', `md-icon-${token}`);
+            }
+
+            nextChildren.push({
               type: 'colored_text',
-              children: [{ type: 'text', value: match[3] }],
+              children: [{ type: 'text', value: match[7] }],
               data: { hName: 'span', hProperties: { className: classNames } },
             });
-          } else if (match[4] && match[5]) {
+          } else if (match[8] && match[9]) {
             nextChildren.push({
               type: 'highlighted_text',
-              children: [{ type: 'text', value: match[5] }],
-              data: { hName: 'span', hProperties: { className: [`md-highlight-${match[4]}`] } },
+              children: [{ type: 'text', value: match[9] }],
+              data: { hName: 'span', hProperties: { className: [`md-highlight-${match[8]}`] } },
             });
-          } else if (match[6]) {
+          } else if (match[10]) {
             nextChildren.push({
               type: 'superscript',
-              children: [{ type: 'text', value: match[6] }],
+              children: [{ type: 'text', value: match[10] }],
               data: { hName: 'sup' },
             });
-          } else if (match[7]) {
+          } else if (match[11]) {
             nextChildren.push({
               type: 'subscript',
-              children: [{ type: 'text', value: match[7] }],
+              children: [{ type: 'text', value: match[11] }],
               data: { hName: 'sub' },
             });
           }
